19c3efa5586b7aba965d306e439221bfd6332c99
Alex <alejandro.munoz@student.uts.edu.au>
2021-02-24 16:04:43 +1100

    Calculator displays numbers and operators

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="calculator">
         <div class="display">{{display}}</div>
-        <div class="button operator">C</div>
+        <div @click="clear" class="button operator">C</div>
         <div class="button operator">+/-</div>
-        <div class="button operator">%</div>
-        <div class="button operator">÷</div>
-        <div class="button">7</div>
-        <div class="button">8</div>
-        <div class="button">9</div>
-        <div class="button operator">×</div>
-        <div class="button">4</div>
-        <div class="button">5</div>
-        <div class="button">6</div>
-        <div class="button operator">-</div>
-        <div class="button">1</div>
-        <div class="button">2</div>
-        <div class="button">3</div>
-        <div class="button operator">+</div>
-        <div class="button zero">0</div>
-        <div class="button ">.</div>
+        <div @click="append('%')" class="button operator">%</div>
+        <div @click="append('÷')" class="button operator">÷</div>
+        <div @click="append('7')" class="button">7</div>
+        <div @click="append('8')" class="button">8</div>
+        <div @click="append('9')" class="button">9</div>
+        <div @click="append('×')" class="button operator">×</div>
+        <div @click="append('4')" class="button">4</div>
+        <div @click="append('5')" class="button">5</div>
+        <div @click="append('6')" class="button">6</div>
+        <div @click="append('-')" class="button operator">-</div>
+        <div @click="append('1')" class="button">1</div>
+        <div @click="append('2')" class="button">2</div>
+        <div @click="append('3')" class="button">3</div>
+        <div @click="append('+')" class="button operator">+</div>
+        <div @click="append('0')" class="button zero">0</div>
+        <div @click="append('.')" class="button">.</div>
         <div class="button operator">=</div>
     </div>
 </template>
@@ -28,7 +28,15 @@ export default {
     name: 'Calculator',
     data() {
         return {
-            display: "0"
+            display: ""
+        }
+    },
+    methods: {
+        clear() {
+            this.display = '';
+        },
+        append(number) {
+            this.display = this.display.concat(number);
         }
     }
 }
@@ -65,6 +73,7 @@ export default {
 .display {
     grid-column-start: 1;
     grid-column-end: 5;
+    text-align: end;
     background-color:cornsilk ;
 }
 

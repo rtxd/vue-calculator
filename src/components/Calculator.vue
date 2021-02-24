ada24cc7f49895301299fe0ee9aa215b1659a6d2
Alex <alejandro.munoz@student.uts.edu.au>
2021-02-24 14:26:17 +1100

    Create Vue Project

---
+++ src/components/Calculator.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="calculator">
+        Hello World
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Calculator',
+}
+</script>
+
+<style scoped>
+
+</style>
